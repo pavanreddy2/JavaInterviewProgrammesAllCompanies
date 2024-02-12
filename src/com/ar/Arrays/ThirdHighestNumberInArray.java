dbab79ace44c0779f12e0f013c7aef6cdb1945ae
pavan0229 <bugganapavan@gmail.com>
2024-02-12 11:52:19 +0530

    Added code and comments on code.

--- src/com/ar/Arrays/ThirdHighestNumberInArray.java
+++ src/com/ar/Arrays/ThirdHighestNumberInArray.java
@@ -1,5 +1,22 @@
 package com.ar.Arrays;
 
-public class ThirdHighestNumberInArray {
+import java.util.Arrays;
 
+public class ThirdHighestNumberInArray {
+	public static void main(String[] args) {
+		int[] array = { 12, 3, 24, 55, 67, 77 };
+		
+		//creates a stream of int values from the given int array array
+		int thirdArray = Arrays.stream(array)
+				//converts each int element of the stream into its corresponding boxed type (Integer).
+				//This is necessary because the subsequent operations (sorted, distinct, etc.) 
+				//work with objects, not primitives.
+						.boxed()
+						.sorted((a, b) -> b - a)
+						.distinct().skip(2)
+						.findFirst()
+						.orElseThrow(() -> new IllegalArgumentException("Array is too short"));
+
+		System.out.println(thirdArray);
+	}
 }
