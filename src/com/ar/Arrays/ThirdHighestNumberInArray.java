602736efaca6b4131fb4aefb00e6ba000652d714
AR Institute <bpavankumar402@gmail.com>
2024-11-06 22:08:28 +0530

    added space

--- src/com/ar/Arrays/ThirdHighestNumberInArray.java
+++ src/com/ar/Arrays/ThirdHighestNumberInArray.java
@@ -6,6 +6,7 @@ public class ThirdHighestNumberInArray {
 	public static void main(String[] args) {
 		int[] array = { 12, 3, 24, 55, 67, 77 };
 
+
 		//creates a stream of int values from the given int array
 		int thirdHighestArray = Arrays.stream(array)
 				//converts each int element of the stream into its corresponding boxed type (Integer).
